fa6122d3cc32aafa99c5b36aeb177f1982b760ce
luoshuai990529 <1029113525@qq.com>
2022-02-16 23:31:29 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
  * @Description: 这是***页面（组件）
  * @Date: 2022-02-07 10:37:58
  * @Author: luoshuai
- * @LastEditors: luoshuai
- * @LastEditTime: 2022-02-14 17:16:30
+ * @LastEditors: Lewis
+ * @LastEditTime: 2022-02-16 23:30:52
 -->
 <template>
-    <div id="app">
-        <router-view></router-view>
-    </div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
-<style>
-
+<style lang="less" scoped>
+#app{
+    width: 100vw;
+    height: 100vh;
+    background: #fff;
+}
 </style>
